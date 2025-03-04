25fd99f0f5d1383ad255143b42dc4fe22291b15a
Ariel Reyes <alreyes2@espe.edu.ec>
2025-03-04 13:26:13 -0500

    first commit

--- Examen-3P/tienda-virtual-frontend/src/app/models/usuario.ts
+++
@@ -1,9 +0,0 @@
-export class Usuario {
-  constructor(
-    public id: string,
-    public nombre: string,
-    public correo: string,
-    public direccion: string,
-    public rol: string
-  ) {}
-}
