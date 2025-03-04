4b8e692de1af0d961e5016fa327c1d4a1527b4e0
Ariel Reyes <alreyes2@espe.edu.ec>
2025-03-04 14:10:45 -0500

    script start

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const PUERTO = process.env.PUERTO || 3000;
+const PUERTO = process.env.PORT || 3000;
 
 require('dotenv').config();
 
@@ -7,6 +7,6 @@ console.log('DB_USER:', process.env.DB_USER);
 console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
 console.log('DB_NAME:', process.env.DB_NAME);
 
-app.listen(PUERTO, () => {
-    console.log(`Servidor corriendo en el puerto ${PUERTO}`);
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
